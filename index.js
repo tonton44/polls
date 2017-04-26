1cd0101ef3211b16238c88c275805243cbe4e6c6
Anthony Gougeon <anthony.gougeon@nantes.imie.fr>
2017-04-26 09:17:41 +0200

    All is ok

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const uuid = require('uuid/v4');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 const polls = [
     {
@@ -19,70 +17,77 @@ const polls = [
         question: "Question 2 ?",
         answers: ["Réponse 1", "Réponse 2", "Réponse 3"],
         votes: [1, 0, 0, 2, 1, 0, 1, 1]
-    },
-    {
-        id: 2,
-        question: "Question 3 ?",
-        answers: ["Réponse 1", "Réponse 2", "Réponse 3"],
-        votes: [1, 0, 0, 2, 1, 0, 1, 1]
-    },
+    }
 ];
 
-// Récupérer la liste des  sondages
+// Lister les sondages
 app.get('/polls', (req, res) => {
     res.send(polls);
 });
 
 // Récupérer un sondage
 app.get('/polls/:id', (req, res) => {
-    // on extrait le paramètre id et on le transforme en nombre
+    // On extrait le paramètre id et on le transforme en nombre
     const id = parseInt(req.params.id, 10);
-    //on cherche le sondage par son id 
-    const poll = polls.find(p => p.id === id)
-    if (typeof (poll) === 'undefined') {
-        //si pas de sondage trouvé on renvoie une erreur 404
-        res.sendStatus(404);
-
-    } else {
-        //sinon un sondage est trouvé et on le renvoie
+    // On cherche le sondage par son id
+    const poll = polls.find(p => p.id === id);
+    if (typeof (poll) !== 'undefined') {
+        // Si un sondage est trouvé, on le renvoie
         res.send(poll);
+    } else {
+        // Sinon, on envoie une erreur 404
+        res.sendStatus(404);
     }
 });
 
 // Créer un sondage
 app.post('/polls', (req, res) => {
-    // On vérifie si question est une chaîne de caractères
-    // const question = req.body.question;
-    // const answers = req.body.answers;
+    //const question = req.body.question;
+    //const answers = req.body.answers;
     const { question, answers } = req.body;
-    // On vérifie si answers est une liste de chaînes de caractères
-    if (!Array.isArray(answers) || answers.some(a => typeof (a) !== 'string')) {
+
+    // On vérifie si question est une chaîne de caractères
+    if (typeof (question) !== 'string') {
         return res.sendStatus(400);
     }
-    // On créer un nouvel identifiant unique, supérieur à tous les autres
+    // On vérifie si answers est une liste de chaînes de caractères
+    if (!Array.isArray(answers) || answers.some(a => typeof (a) !== 'string') ||
+        answers.length < 2) {
+        return res.sendStatus(400);
+    }
+
+    // On crée un nouvel identifiant unique, supérieur à tous les autres
+    //const id = Math.max.apply(null, polls.map(p => p.id)) + 1;
     const id = polls.reduce((max, p) => max > p.id ? max : p.id, 0) + 1;
 
-
-    // on créé un nouvel objet sondage
+    // On crée un nouvel objet sondage
     const poll = {
         id, question, answers,
         votes: []
     };
-    // on ajoute le nouveau sondage à la liste
 
+    // On ajoute le nouveau sondage à la liste
     polls.push(poll);
 
-    // et on renvoie avec le bon code HTTP
-    res.send(201, polls);
+    // et on le renvoie avec le bon code HTTP
+    res.send(201, poll);
 });
 
+// Voter pour une réponse d'un sondage
+app.post('/polls/:id/votes', (req, res) => {
+    // On récupère le sondage par son id
 
+    // On envoie une erreur si le sondage n'existe pas
 
-// Récupérer un sondage
-app.get('/', function (req, res) {
-    res.send('Hello World !!')
-})
-// Récupérer un sondage
+    // On récupère l'index de la réponse votée depuis le body
+    // et on vérifie sa validité
+
+    // On ajoute l'index de la réponse à la liste des votes du sondage
+
+    // On renvoie la nouvelle liste des votes
+
+});
+
 app.listen(3000, () => {
-    console.log("listening on port 3000")
-})
+    console.log('Listening on port 3000');
+});
